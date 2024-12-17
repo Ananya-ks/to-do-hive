e9aa475fd1eab8e0abcd24767f3d66c06d6d8604
Ananya <anusatheesh1905@gmail.com>
2024-12-17 11:07:45 +0530

    check on new branch 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import './pages/pages.dart';
 
 // sample to-do branch
+//additional
 void main()async{
   /// initiate hive flutter
   await Hive.initFlutter();
